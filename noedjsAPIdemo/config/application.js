c1c9da7e5bd29b644efede2099b17507d517e1a6
shijiangang <s44506894@gmail.com>
2018-10-18 15:11:27 +0800

    添加回调地址

--- noedjsAPIdemo/config/application.js
+++ noedjsAPIdemo/config/application.js
@@ -1,6 +1,11 @@
 module.exports = {
-    'apiHost':'https://api.cryptocurpays.com/api/v1/',
-    'appId':100013,
-    'appSecret':'koHfMgSBuvlouq5CELZb4gH92uZX3QxU5gnG0bVI9WwXExBI01bilglBGhXm0r1XyFg41hXEFSwyn1o40I5awhgxM7aGdir5gdfHILcB1kF7LKkD3NYzaT9Se3RT42ZtB6MUlrvTceexMvnW4tKyLfe8vPV93BMvRUFenlC9X45T3zZy1XSE4cqIshpYdfEU4o24yP1e',
+    // 'apiHost':'https://api.cryptocurpays.com/api/v1/',
+    // 'appId':100013,
+    // 'appSecret':'koHfMgSBuvlouq5CELZb4gH92uZX3QxU5gnG0bVI9WwXExBI01bilglBGhXm0r1XyFg41hXEFSwyn1o40I5awhgxM7aGdir5gdfHILcB1kF7LKkD3NYzaT9Se3RT42ZtB6MUlrvTceexMvnW4tKyLfe8vPV93BMvRUFenlC9X45T3zZy1XSE4cqIshpYdfEU4o24yP1e',
+    'apiHost':'https://api.cryptocurpays.com:8080/api/v1/',
+    'appId':153,
+    'appSecret':'lRxHaZ0fdXaiiTJCgCp4H5HlN2Fom9OdPEEl5grtYf5PZs0lVGOF0ZyuXEaT7adRLqzsCegpw2YqPeEiv8EDbxTLGPQrkWPOHyOGHHMExscZ2oayE' +
+    'OXhNp1a9KADlyZhZRUeig08OYSEKWHHlmFXca1IELbI9MRnxTyS1wcMJLV3c8GOQn9W3PmgQQUBCr0QfnqYcWqI',//'koHfMgSBuvlouq5CELZb4gH92uZX3QxU5gnG0bVI9WwXExBI01bilglBGhXm0r1XyFg41hXEFSwyn1o40I5awhgxM7aGdir5gdfHILcB1kF7LKkD3NYzaT9Se3RT42ZtB6MUlrvTceexMvnW4tKyLfe8vPV93BMvRUFenlC9X45T3zZy1XSE4cqIshpYdfEU4o24yP1e',
+
     'depositSignedAddress':'1KS343xEEJPhqZmJeUzs5LhNoniBTDx38G'
 };
